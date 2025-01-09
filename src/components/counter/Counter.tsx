7c7f1fa0a9e321340621cdf54643894251ea6109
roma1524 <kulikr.js@mail.ru>
2025-01-09 12:01:41 +0300

    structure

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Counter.css'
+
+export const Counter = () => {
+    return (
+        <div className='counter'>
+
+        </div>
+    );
+};
+
---
+++ src/components/settingOfCounter/SettingOfCounter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SettingOfCounter.css'
+
+export const SettingOfCounter = () => {
+    return (
+        <div className='setting-of-counter'>
+
+        </div>
+    );
+};
+
